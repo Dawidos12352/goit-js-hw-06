6193a097562beef43df3de8da5984567d1187113
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-05 17:58:14 +0100

    1 js done
    finish first task in js

---
+++ src/js/task-01.js
@@ -0,0 +1,24 @@
+// first attempt at executing a task without using a loop, result is correct
+
+
+/*
+const numberOfCategories = document.querySelectorAll('#categories>li.item');
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+console.log(`Category: ${document.body.querySelectorAll("h2")[0].innerText}`);
+console.log(`Elements: ${document.body.querySelectorAll("ul")[1].childElementCount}`);
+
+console.log(`Category: ${document.body.querySelectorAll("h2")[1].innerText}`);
+console.log(`Elements: ${document.body.querySelectorAll("ul")[2].childElementCount}`);
+
+console.log(`Category: ${document.body.querySelectorAll("h2")[2].innerText}`);
+console.log(`Elements: ${document.body.querySelectorAll("ul")[3].childElementCount}`);
+
+*/
+
+const numberOfCategories = document.querySelectorAll('#categories > li.item');
+console.log(`Number of categories: ${numberOfCategories.length}`);
+numberOfCategories.forEach( e => {
+    console.log(`Category: ${e.firstElementChild.textContent}`);
+    console.log(`Elements: ${e.lastElementChild.children.length}`);
+});
---
+++ src/js/task-02.js
@@ -0,0 +1,10 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const list = document.querySelector("ul#ingredients");
+const itemList = ingredients.map( e =>)
