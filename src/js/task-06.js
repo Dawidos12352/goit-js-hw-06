155c41d1b51507d227998adc0c0d4a0dc9280921
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-06 19:58:04 +0100

    reset all

--- src/js/task-06.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector("input#validation-input");
-
-input.addEventListener("blur", (event) => {
-    if(event.currentTarget.value.length !== 6)
-    event.currentTarget.setAttribute("class", "invalid");
-    else
-    event.currentTarget.setAttribute("class", "valid");
-    });
--- src/js/task-07.js
+++
@@ -1,4 +0,0 @@
-const input = document.querySelector("input#font-size-control");
-const text = document.querySelector("span#text");
-input.addEventListener("input", (event) => 
-text.style.fontSize = `${event.currentTarget.value}px`);
