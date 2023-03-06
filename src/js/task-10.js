81405186b2c942d5dbb489fea15d72de98202fab
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-06 19:25:40 +0100

    10 done (optional)

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector(`button[data-create=""]`);
+const btnDestroy = document.querySelector(`button[data-destroy=""]`);
+const input = document.querySelector("input");
+const boxes = document.querySelector("div#boxes");
+
+btnCreate.addEventListener( "click", () => createBoxes(input.value));
+btnDestroy.addEventListener("click", () => destroyBoxes());
+
+const destroyBoxes = () => { boxes.textContent = "" }; 
+
+
+function createBoxes(amount){
+
+  let boxHeight = 30;
+  let boxWidth = 30;
+
+for (let i = 0; i < amount; i++) {
+
+  const box = document.createElement("div");
+  box.classList.add("box");
+  box.style.height = `${boxHeight + (i * 10)}px`;
+  box.style.width = `${boxWidth + (i * 10)}px`;
+  box.style.backgroundColor = `${getRandomHexColor()}`;
+  boxes.prepend(box);
+}
+};
