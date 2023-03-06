532d94bef955a897fafc66f7e934daa6d624361d
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-06 20:51:45 +0100

    2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul#ingredients");
+
+const listOfIngredients = ingredients.map( event => {
+  const li = document.createElement("li");
+  li.textContent = event;
+  li.classList.add("item");
+  return li
+});
+
+list.append(...listOfIngredients);
+console.log(list);
